4ffbef3f59ba648586f64635dee25615faf471f9
Paul Vorbach <paul@vorb.de>
2013-02-08 13:48:39 +0100

    Add menu bar with about dialog

--- src/main/scala/de/vorb/vision/binarization/GUI.scala
+++ src/main/scala/de/vorb/vision/binarization/GUI.scala
@@ -1,14 +1,16 @@
 package de.vorb.vision.binarization
 
-import java.awt.{ Color, Desktop }
+import java.awt.{ Color, Cursor, Desktop }
 import java.awt.image.BufferedImage
 import java.io.{ File, IOException }
-import scala.swing.{ BoxPanel, Button, CheckBox, ComboBox, Component, Dialog, FileChooser, FlowPanel, Label, MainFrame, Orientation, SimpleSwingApplication, TextField }
+import scala.swing.{ BoxPanel, Button, CheckBox, Component, Dialog, FileChooser, FlowPanel, Label, MainFrame, MenuBar, Orientation, SimpleSwingApplication, TextField }
 import scala.swing.event.ButtonClicked
 import javax.imageio.ImageIO
 import javax.swing.{ ImageIcon, JOptionPane, UIManager }
 import javax.swing.filechooser.FileFilter
-import java.awt.Cursor
+import scala.swing.Menu
+import scala.swing.MenuItem
+import scala.swing.Action
 
 object GUI extends SimpleSwingApplication {
   try {
@@ -20,6 +22,35 @@ object GUI extends SimpleSwingApplication {
     try {
       iconImage = ImageIO.read(getClass.getResource("/logo.png"))
     } catch { case _: Throwable => }
+
+    menuBar = new MenuBar {
+      contents += new MenuItem(Action("About") {
+        Dialog.showMessage(menuBar,
+          """Copyright © 2013 Paul Vorbach <paul@vorb.de>
+            |
+            |Permission is hereby granted, free of charge, to any person
+            |obtaining a copy of this software and associated documentation
+            |files (the “Software”), to deal in the Software without
+            |restriction, including without limitation the rights to use, copy,
+            |modify, merge, publish, distribute, sublicense, and/or sell copies
+            |of the Software, and to permit persons to whom the Software is
+            |furnished to do so, subject to the following conditions:
+            |
+            |The above copyright notice and this permission notice shall be
+            |included in all copies or substantial portions of the Software.
+            |
+            |THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF
+            |ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT
+            |LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+            |FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO
+            |EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+            |LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+            |WHETHER IN AN ACTION OF CONTRACT, TORT OR
+            |OTHERWISE, OUT OF OR IN CONNECTION WITH THE SOFTWARE
+            |OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.""".stripMargin,
+          "About the software")
+      })
+    }
 
     val invalid = new Color(0xFFFFAAAA)
     val valid = new Color(0xFFAAFFAA)
